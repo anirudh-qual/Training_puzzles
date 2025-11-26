b6ba8a8e72adf4764299631441b3f29cd5b63d43
Venkata Sai Anirudh Kamaraj Vobbilisetty <vvobbilisetty6@gatech.edu>
2025-11-25 21:13:35 -0500

    part1

--- lab2-main/part1.py
+++ lab2-main/part1.py
@@ -34,15 +34,17 @@ async def part1_training_loop(model: Model, batch_size: int) -> Model:
         for l in range(model.num_layers):
             weights[l] = await model.all_gather(weights_sharded[l],l)
             activations[l + 1] = model.forward(l, activations[l], weights[l])
+            del weights[l]
         
         grad_activations[model.num_layers] = model.loss(activations[model.num_layers])
         for l in range(model.num_layers - 1, -1, -1):
+            weights[l] = await model.all_gather(weights_sharded[l],l)
             grad_weights[l], grad_activations[l] = model.backward(
                 l, activations[l], grad_activations[l + 1], weights[l]
             )
             
             del grad_activations[l + 1], activations[l], weights[l]
-            g_shard =await model.reduce_scatter(grad_weights[l],l)
+            g_shard = await model.reduce_scatter(grad_weights[l],l)
             if grad_weights_sharded[l] == None:
                 grad_weights_sharded[l]=g_shard
             else:
@@ -59,3 +61,44 @@ async def part1_training_loop(model: Model, batch_size: int) -> Model:
         )
         model.set_final_weight(l,weights[l])
     return model
+
+async def main():
+    world_size = 8
+    num_layers = 36
+    global_batch_size = 32
+    batch_size = 4
+
+    dist = Dist(world_size)
+    models: List[Model] = [
+        Model(
+            num_layers=num_layers,
+            global_batch_size=global_batch_size,
+            rank=i,
+            dist=dist,
+        )
+        for i in range(world_size)
+    ]
+
+    theoretical_best_time = get_model_theoretical_best_time(models[0])
+
+    out = await asyncio.gather(
+        *[
+            part1_training_loop(
+                models[i],
+                batch_size,
+            )
+            for i in range(world_size)
+        ]
+    )
+
+    time, memory, max_mem_rank = get_training_stats(out)
+
+    mfu = (theoretical_best_time / time) * 100
+
+    print(mfu)
+
+    write_chrome_trace(out, "./debug_traces/part1.json")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
